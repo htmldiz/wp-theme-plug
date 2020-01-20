9c05e63a78afa2fb886f309f942153ac944c5361
htmldiz <mark.tanasevich@gmail.com>
2020-01-20 15:11:49 +0300

    add plugins

---
+++ wp-theme-plug.php
@@ -0,0 +1,21 @@
+<?php 
+/*
+Plugin Name: WP theme plug
+Plugin URI: 
+Description: 
+Version: 1.0
+Author: Markus {code}
+Author URI:
+*/
+
+require_once("lib/meta-box-class/my-meta-box-class.php");
+require_once("lib/CPT.php");
+class ThemeSettingsCL
+{
+	
+	function __construct()
+	{
+		
+	}
+}
+new ThemeSettingsCL();
